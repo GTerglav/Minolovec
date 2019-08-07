f9b05677e8a1e8b31f01220ebb35267de0aebab8
GTerglav <gasper.terglav@gmail.com>
2019-08-07 20:39:38 +0200

    zacel delo na modelu

--- model.py
+++ model.py
@@ -8,10 +8,18 @@ import random, time, copy
 #polje 9*9
 rešeno_polje9 = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+
+def naredi_dombo(sez):
+    vrstica = random.randint(0,8)
+    stolpec = random.randint(0,8)
+    if not rešeno_polje9[vrstica][stolpec] == "*":
+        rešeno_polje9[vrstica][stolpec] = "*"
+
+
 def postavi_bombe(self):
     for n in range(10):
         naredi_bombo(rešeno_polje9)
-        
+
         
 
 
