db70889cb3b741680db0d73c2b2a6c56555a8785
GTerglav <gasper.terglav@gmail.com>
2019-08-09 12:45:42 +0200

    dodal funkcijo ki prešteje okoliške mine

--- model.py
+++ model.py
@@ -50,6 +50,36 @@ class Polje:
                                         return True
                 return False
 
+        def zmaga(self):
+                for i in range(len(self.seznam)):
+                        for j in self.seznam[i]:
+                                if j.mina == False and j.vidna == False:
+                                        return False
+                return True
+        
+
+        def preštej_mine(self, vrsta, stolpec):
+                vsota = 0
+                for x_os, y_os in self.sosedi(vrsta, stolpec):
+                        if self.je_dovoljena(x_os, y_os):
+                                if self.seznam[x_os][y_os].mina == True:
+                                        vsota += 1
+                return vsota
+
+        def sosedi(self, vrsta, stolpec):
+                sez = []
+                okolica = ((-1, -1), (-1,  0), (-1,  1),
+                           (0 , -1),           (0 ,  1),
+                           (1 , -1), (1 ,  0), (1 ,  1))
+                for premik_x, premik_y in okolica:
+                        sez.append((vrsta + premik_x, stolpec + premik_y))
+                return sez 
+        
+        
+        def je_dovoljena(self, vrsta, stolpec):
+                return 0 <= vrsta <= len(self.seznam) - 1 and 0 <= stolpec <= len(self.seznam) - 1
+        
+
 
 def naredi_polje(velikost, mine):
         #naredi matriko
@@ -61,9 +91,7 @@ def naredi_polje(velikost, mine):
                 for j in range(velikost):
                         vrstica.append(Celica(False))
                 matrika.append(vrstica)
-        
-        polje = Polje(matrika)
-        
+               
         #postavi mine
         sez_moznosti = []
         for i in range(velikost):
@@ -77,8 +105,14 @@ def naredi_polje(velikost, mine):
                 nov_poz = random.choice(sez_moznosti)
                 sez_moznosti.remove(nov_poz)
                 matrika[nov_poz[0]][nov_poz[1]].postavi_mino()
+        
+        polje = Polje(matrika)
         return matrika
         
-
+def razkrij_vse(sez):
+        for i in range(len(sez)):
+                for j in range(len(sez)):
+                        sez[i][j].razkrij()
+        return
 
 
